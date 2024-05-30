8527cc9637ce0ee7d83360fce4dcc271556ef10c
11160862xuan <114331586+11160862xuan@users.noreply.github.com>
2024-05-30 16:40:01 +0800

    week15

---
+++ week15-2_PlaySound_empty/main.cpp
@@ -0,0 +1,11 @@
+///把程式碼刪掉
+#include <windows.h>
+#include <stdio.h>
+int main()
+{
+    ///PlaySound("C:/Do.wav",NULL,SND_SYNC);
+    PlaySound("C:/Do.wav",NULL,SND_SYNC);
+    printf("請輸入一個整數: ");
+    int n;
+    scanf("%d", &n);
+}
